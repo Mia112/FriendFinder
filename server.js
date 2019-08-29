6dedb94442dbcf463aa1fe1f88c3f52dfe989b69
Mia112 <msheriff112@gmail.com>
2019-08-28 22:12:39 -0400

    updates

--- server.js
+++ server.js
@@ -3,26 +3,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
- apiRoutes = require("./app/routing/apiRoutes.js")(app);
- htmlRoutes = require("./app/routing/htmlRoutes.js")(app);
-
 // require('./app/routing/apiRoutes.js')(app);
 // require('./app/routing/htmlRoutes.js')(app);
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
 
 app.use(bodyParser.json());
-app.use('/assets', express.static('assets'));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+// app.use('/assets', express.static('assets'));
 app.use(express.static('app/public'));
 
 
 // app.use(apiRoutes);
 // app.use(htmlRoutes);
+
+ apiRoutes = require("./app/routing/apiRoutes.js")(app);
+ htmlRoutes = require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, () => console.log("App listening on PORT " + PORT));
 
