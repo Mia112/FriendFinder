079ad455e8efeace7407596587ed0745e6d2128a
Mia112 <msheriff112@gmail.com>
2019-09-04 17:19:49 -0400

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,63 +1,64 @@
-// var path = require("path")
-// var friends = require("../app/data/friends");
+
 var friends = require('../data/friends.js');
 
 module.exports = function (app) {
+   // API GET a Request
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-
-    module.exports = function(app) {
-
-      app.get("/api/friends", function(req, res) {
-        res.json(friends);
-      });
     
-      // handle the post request 
+ 
+      // user input get submited to the server.
       app.post("/api/friends", function(req, res) {
-        
-        // begin by setting up the array hoding the user's answers
-        var newFriend = req.body.scores;
-        // convert the values in surveyResults to integers
-        for (var i=0; i<newFriend .length; i++) {
-          newFriend [i] = parseInt(newFriend [i]);
-        }
-    
-      
-        var bestDifference = 999999;
-        var bestMatch = 0; 
-       
-        for (i=0; i < friends.length; i++) {
-    
-        
-          var tempDifference = difference(newFriend , friends[i].scores);
-    
-          // console log the difference between user choices and pet being compared
-          console.log("difference between", newFriend , "and", friends[i].name, friends[i].scores, "=", tempDifference);
-    
-       
-          if (tempDifference < bestDifference) {
-            bestDifference = tempDifference;
-            bestMatch = i;
-          }
-        }
-    
-       
-        function difference(array1, array2) {
-    
-          
-          var differenceAmount=0;
-          
-          for (var i=0; i<array1.length; i++) {
-            differenceAmount += Math.abs(array1[i] - array2[i]);
-          }
-          
-          return differenceAmount;
-        }
-    
-        res.send(friends[bestMatch]);
-      });
+
+        console.log(req.body)
+    // this empty object will hold the "best match". 
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
     };
 
-} 
+    var newFriend = req.body;
+    var userScores = newFriend.scores;
+
+
+    // This variable will calculate the difference between the user"s scores and the scores of
+    // each user in the database
+    var totalDifference = 0;
+
+    // Here we loop through all the friend possibilities in the database.
+    for (var i = 0; i < friends.length; i++) {
+
+      
+      totalDifference = 0;
+
+       // We then loop through all the scores of each friend
+       for (var j = 0; j < friends[i].scores[j]; j++) {
+
+        // We calculate the difference between the scores and sum them into the totalDifference
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+        // If the sum of differences is less then the differences of the current "best match"
+        if (totalDifference <= bestMatch.friendDifference) {
+
+          // Reset the bestMatch to be the new friend.
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.friendDifference = totalDifference;
+          
+        }
+      }
+    }
+     // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+    // the database will always return that the user is the user's best friend).
+    friends.push(newFriend);
+
+  
+    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+    res.render(bestMatch);
+
+  });
+
+};
         
--- server.js
+++ server.js
@@ -10,12 +10,13 @@ var app = express();
 // create a port to be sent by Heroku 
 var PORT = process.env.PORT || 8080;
 
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(express.urlencoded())
+// app.use(express.urlencoded())
 //makes static assets in the public folder available (style.css)
 app.use(express.static("app/public"));
 
